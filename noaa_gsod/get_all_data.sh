833e8d16ed6e68ac1ffa1e9c8ea47ae72e3bd02e
nick <nick@goggl.es>
2014-02-05 18:40:29 -0600

    Identify get_all_data.sh as a bash script.

--- noaa_gsod/get_all_data.sh
+++ noaa_gsod/get_all_data.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Although there are folders for years 1901 to 1928 on the FTP site, the archive files there are empty.
 for year in {1929..2014}; do
     ./get_data_for_year_XXXX.sh $year
