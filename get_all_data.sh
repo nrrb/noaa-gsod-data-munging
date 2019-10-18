89761fb23ef0b24967344146aba93a54412723bc
Nicholas Bennett <nick271828@gmail.com>
2019-10-18 10:58:45 -0500

    Update get_all_data.sh

--- get_all_data.sh
+++ get_all_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Get the measurement station list, which is keyed by STN and WBAN numbers in the data files
+./get_station_list.sh
 # Although there are folders for years 1901 to 1928 on the FTP site, the archive files there are empty.
 for year in {1929..2019}; do
     ./get_data_for_year_XXXX.sh $year
